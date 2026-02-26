24e2feb7e68e36061b1bb93fcdca0b15007bd0fe
Juan Chica <jochop1504@icloud.com>
2026-02-25 22:36:53 -0500

    add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function navSmoothScroll() {
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href'))
+              .scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+
+navSmoothScroll();
+}
